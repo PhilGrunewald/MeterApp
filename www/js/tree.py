787401247d27d882e836035f99e244052f107098
Phil Grunewald <philipp.grunewald@ouce.ox.ac.uk>
2016-03-14 08:17:18 +0000

    recursion stop at leaves

--- www/js/tree.py
+++ www/js/tree.py
@@ -4,24 +4,16 @@ acts = json.loads(datafile.read().decode("utf-8"))
 datafile = open('/Users/phil/Sites/MeterApp/www/js/screens.json', 'r')
 screens = json.loads(datafile.read().decode("utf-8"))
 
-level = 0
-def recursive(node):
-    global level
-    level += 1
+def recursive(node,level):
     for branch in screens['screens'][node]['activities']:
             nextNode = acts['activities'][branch]['next']
-            print '\t'*level + node + ' > ' + branch + ' > ' + nextNode
-            if (nextNode == 'home'):
-                print '## end home'
-                level = 0
-                break
-            if (nextNode == 'enjoyment'):
-                level = 0
-                break
-            try:
-                recursive(nextNode)
-            except KeyError:
-                print 'ERROR at: ' + node + ' > ' + branch + ' > ' + nextNode
+            print '.'*level + node + ' > ' + branch + ' > ' + nextNode
+            if not ((nextNode == 'location') or (nextNode == 'home') or (nextNode == 'enjoyment')):
+                try:
+                    level += 1
+                    recursive(nextNode,level)
+                except KeyError:
+                    print 'ERROR at: ' + node + ' > ' + branch + ' > ' + nextNode
 
-recursive('home')
+recursive('home',0)
     
