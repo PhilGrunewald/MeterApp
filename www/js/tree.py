da6c724296421112578703d4062784b76a2754e0
Phil Grunewald <philipp.grunewald@ouce.ox.ac.uk>
2016-03-15 10:22:47 +0000

    removed dead leaves

--- www/js/tree.py
+++ www/js/tree.py
@@ -5,15 +5,17 @@ datafile = open('/Users/phil/Sites/MeterApp/www/js/screens.json', 'r')
 screens = json.loads(datafile.read().decode("utf-8"))
 
 def recursive(node,level):
+    level += 1
     for branch in screens['screens'][node]['activities']:
             nextNode = acts['activities'][branch]['next']
-            print '.'*level + node + ' > ' + branch + ' > ' + nextNode
+            caption = acts['activities'][branch]['caption']
+            # print '.'*level + node + ' > ' + branch + ' > ' + nextNode
+            print '.   '*level + caption
             if not ((nextNode == 'location') or (nextNode == 'home') or (nextNode == 'enjoyment')):
                 try:
-                    level += 1
                     recursive(nextNode,level)
                 except KeyError:
                     print 'ERROR at: ' + node + ' > ' + branch + ' > ' + nextNode
 
-recursive('home',0)
+recursive('home',-1)
     
