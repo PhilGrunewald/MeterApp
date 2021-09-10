e84f80806d2b39e42379f978d440ec68dbf6d0ab
Phil Grunewald <philipp.grunewald@ouce.ox.ac.uk>
2021-09-10 22:03:05 +0100

    delete issue fixed with async await!

--- www/js/index.js
+++ www/js/index.js
@@ -1,27 +1,8 @@
 /*
 * Meter Activity App
 */
-
-if (localStorage.getItem('language') == null) {
-    var localLanguage = navigator.language.split("-")[0]
-    if (localLanguage == 'de') {
-        localStorage.setItem('language', localLanguage);
-        console.log("Language is " + localLanguage + " - will use German")
-    } else {
-        if (navigator.language.split("-")[1] == 'US') {
-            localStorage.setItem('language', 'us');
-            console.log("Language is " + localLanguage + " - will use U.S. English")
-        } else {
-        localStorage.setItem('language', 'en');
-        console.log("Language is " + localLanguage + " - will use English")
-        }
-    }
-}
-
 var appVersion = "1.1.12";
 var meterURL = "https://www.energy-use.org/app/"
-
-// var CATEGORIES = ["new", "function", "assign", "care_self", "care_other", "care_house", "recreation", "travel", "food", "work", "other_category", "blank","location", "people", "enjoyment"];
 
 var app = {
   initialize: function() {
@@ -32,28 +13,37 @@ var app = {
     document.addEventListener('deviceready', this.onDeviceReady, false);
   },
 
-  onDeviceReady: function() {
-      app.initialSetup();
-      app.loadText();
-  },
+onDeviceReady: function() {
+  app.initialSetup();
+  app.loadText();
+},
 
-  loadJSON: async function(jsonName,language) {
-    //if (localStorage.getItem(jsonName) == null) {
-      const response = await fetch(`text/${jsonName}-${language}.json`)
-      // XXX const response = await fetch(`${meterURL}/json/${jsonName}.json`)
-      const json     = await response.json();
-      localStorage.setItem(jsonName, JSON.stringify(json));
-    //}
-   return JSON.parse(localStorage.getItem(jsonName));
-  },
+loadJSON: async function(jsonName,language) {
+    if (localStorage.getItem(jsonName) == null) {
+        const response = await fetch(`text/${jsonName}-${language}.json`)
+        // XXX const response = await fetch(`${meterURL}/json/${jsonName}.json`)
+        const json     = await response.json();
+        localStorage.setItem(jsonName, JSON.stringify(json));
+    }
+    return JSON.parse(localStorage.getItem(jsonName));
+},
 
-  loadText: async function() {
-    const language = localStorage.getItem('language');
+loadText: async function() {
+    var language = navigator.language.split("-")[0]
+    switch(language) {
+      case 'de':
+        localStorage.setItem('language', localLanguage);
+      case 'US':
+        localStorage.setItem('language', 'us');
+      default:
+        localStorage.setItem('language', 'en');
+    }
+    language = localStorage.getItem('language');
     [app.label, app.activities, app.screens] = await Promise.all([
         app.loadJSON('labels',language),
         app.loadJSON('activities',language),
         app.loadJSON('screens',language)
-        ]); // .then(values => { console.log("VALUES XX", values); });
+        ]);
 
     if (localStorage.getItem("customActivities") != null) {
         // custom activities have been assigned - these overwrite the default activities
@@ -68,63 +58,27 @@ var app = {
     if (localStorage.getItem("acts") == null) {
         localStorage.setItem("acts", JSON.stringify({}));
         localStorage.setItem('key', 0);
+        localStorage.setItem('index', 0);
     }
 
     // CONSENT?
     if (localStorage.getItem("consent") == 1) {
-      // XXX app.navigateTo("menu") 
-      app.moveTo("menu");
+        app.moveTo("menu");
     } else {
-        app.navbar.hide();
-        app.server.attr('src', 'https://www.energy-use.org/app/consent.php');
-        app.server.show(); 
+        $('#navbar').hide();
+        $('#server').attr('src', 'https://www.energy-use.org/app/consent.php');
+        $('#server').show(); 
     }
 
-        $('#dataPolicyButton').html(app.label.dataPolicyButton);
-        $('#actListLabel').html(app.label.actListLabel);
-        $('#lblPersonalise').html(app.label.lblPersonalise);
-        $('#lblSurvey').html(app.label.lblSurvey);
-        $('#lblBack').html(app.label.lblBack);
-        $('#lblHome').html(app.label.lblHome);
-        $('#lblNext').html(app.label.lblNext);
-        $('#lblDone').html(app.label.lblDone);
-        $('#btn-other-specify').html(app.label.btnOther);
-        $('#progress-authorise').html(app.label.Authorise)
-        $('#help-btn-now').html(app.label.help.btnNow);
-        $('#help-btn-recent').html(app.label.help.btnRecent);
-        $('#help-act-list').html(app.label.help.actList);
-        $('#help-post-code').html(app.label.help.postcode);
-        $('#help-contact-details').html(app.label.help.contactDetails);
-        app.updateNowTime();
-        setInterval(function(){ app.updateNowTime(); }, 10000); // 10 second clock update
+    $('#actListLabel').html(app.label.actListLabel);
+    $('#lblBack').html(app.label.lblBack);
+    $('#lblHome').html(app.label.lblHome);
+    $('#lblNext').html(app.label.lblNext);
+    $('#lblDone').html(app.label.lblDone);
+    setInterval(function(){ app.updateNowTime(); }, 10000); // 10 second clock update
    },
 
   initialSetup: function() {
-    app.actionButtons        = $('.btn-activity');
-    app.activity_list_div    = $('#activity-list');
-    app.activityAddPane      = $('#activity_add_pane');
-    app.activityListPane     = $('#activity_list_pane');
-    app.choicesPane          = $('#choices_pane');
-    app.title                = $("#title");
-    app.header               = $("#header");
-    app.now_time             = $("#now-time");
-    app.helpCaption          = $(".help-caption");
-    app.footerNav            = $("div.footer-nav");
-    app.btnCaption           = $(".btn-caption");
-    app.imgStatus            = $("#imgStatus");
-    app.personaliseScreen    = $('#personalise_screen');
-    app.appScreen            = $('#app_screen');
-    app.btnSubmit            = $('#btn_submit');
-    app.postcodeInput        = $('#postcode_input');
-    app.postcodeSection      = $('#postcode_section');
-    app.helpText             = $('#help_text');
-    app.addressList          = $('#address_list');
-    app.contact_screen       = $('#contact_screen');
-    app.disabled_list_option = $('#disabled_list_option');
-    app.server               = $('#server'); // iframe displaying server side php
-    app.navbar               = $('#navbar');
-
-
     // The clock face behind the "Now" button
     clock.nowClock        = $('#clock-now');
     clock.initClock(clock.nowClock);
@@ -139,61 +93,8 @@ var app = {
     clock.initClock(clock.actClock);
     clock.actClock.hide();
     clock.actClockDiv.hide();
-
-    app.helpCaption.hide(); // hide help text when moving on (default off);
     app.history          = new Array();
     app.act_path         = new Array();
-},
-
-
-statusCheck: function() {
-    // steps are                    condition
-    // 1: personalise               hh id
-    // 2: finish personalise        registration status = 'complete'
-    // 3: get date                  dateChoice
-    // 4: survey                    SURVEY_STATUS
-    // 5: study (stars);
-    // 6: return eMeter             date > dateChoice +1
-    
-    var dateChoice = localStorage.getItem('dateChoice');
-    var hh = localStorage.getItem('household_id');
-    var sc = localStorage.getItem('sc');
-    var waitForAuthorisation = localStorage.getItem('AwaitAuthorisation');
-
-  // Individual Survey
-  if (localStorage.getItem("survey root") == 'survey complete') {
-    app.activities['IndividualSurvey']['icon'] = "act3_user_green";
-  }
-
-  if (localStorage.getItem("householdSurvey") == 'COMPLETE') {
-    app.activities['Register']['icon'] = "act3_home_green";
-    if (localStorage.getItem("survey root") == 'survey complete') {
-        app.imgStatus.attr("src","img/act3_user_green.png");
-    }
-  }
-},
-
-showStars: function() {
-    // activityList = utils.getList(ACTIVITY_LIST) || {};
-    // var actCount = Object.keys(activityList).length;
-    var actCount = localStorage.getItem('ActivityCount');
-    if (actCount > 4) {
-          app.imgStatus.attr("src","img/act3_user_star.png");
-    }
-    //if (actCount > 24) {
-    //  app.imgStatus.attr("src","img/stars_5.png");
-    //} else if (actCount > 14) {
-    //  app.imgStatus.attr("src","img/stars_4.png");
-    //} else if (actCount > 9) {
-    //  app.imgStatus.attr("src","img/stars_3.png");
-    //} else if (actCount > 4) {
-    //  app.imgStatus.attr("src","img/stars_2.png");
-    //} else if (actCount > 0) {
-    //  app.imgStatus.attr("src","img/stars_1.png");
-    //} else {
-    //  app.imgStatus.attr("src","img/stars_0.png");
-    //}
-    //app.imgStatus.show();
 },
 
 updateNowTime: function() {
@@ -239,13 +140,15 @@ moveFrom: function(from) {
     var   key = localStorage.getItem('key');
     var  acts = JSON.parse(localStorage.getItem('acts'));
 
-    console.log(act.category);
+    console.log("move from: ",act.category);
     switch(act.category) {
         case "abort":
             app.deleteAct(key); // bin current entry
+            break;
         case "new":
             // high-precision time as key
-            key = performance.now().toString();
+            localStorage.setItem('index', parseInt(localStorage.getItem('index'))+1);
+            key = `${Number(new Date())}_${localStorage.getItem('index')}`;
             localStorage.setItem('key',key);
             acts[key] = {};
             acts[key]['dt_recorded'] = new Date();
@@ -255,7 +158,13 @@ moveFrom: function(from) {
             break;
         case "time":
             var dt = new Date(acts[key]['dt_activity']);
-            acts[key]['dt_activity'] = new Date(dt.getTime() + (60000*act.value));
+            dt = new Date(dt.getTime() + (60000*act.value));
+            acts[key]['dt_activity'] = dt;
+            var newKey = `${Number(dt)}_${localStorage.getItem('index')}`;
+            acts[newKey] = acts[key];
+            app.deleteAct(key);
+            key = newKey;
+            localStorage.setItem('key',key);
             break;
         case "location":
             acts[key]['location'] = act.value;
@@ -299,7 +208,6 @@ defaultView: function() {
     $('#server').hide();
     $('#clocks').hide();
     $('#list').hide();
-
     $('#header').show();
     $('#buttons').show();
     $('#footer').hide();
@@ -308,7 +216,7 @@ defaultView: function() {
 moveTo: function(to) {
     const screen = app.screens[to];
     app.defaultView();
-    app.title.html(app.formatString(screen.title));
+    $('#title').html(app.formatString(screen.title));
     app.history.push(to);        // for 'back' functionality
     console.log(`move to ${to}`);
     switch(to) {
@@ -358,9 +266,9 @@ moveFromTo: function(from, to) {
     app.moveTo(to);
 },
 
-deleteAct: function(key) {
+deleteAct: async function(key) {
     var acts = JSON.parse(localStorage.getItem('acts'));
-    delete acts[key];
+    await delete acts[key];
     localStorage.setItem('acts',JSON.stringify(acts));
 },
 
@@ -417,7 +325,11 @@ changeEnj: function(key) {
 listActivities: function() {
   document.getElementById('list').innerHTML = ''; // start afresh
   var acts = JSON.parse(localStorage.getItem('acts'));
-  for (var key in acts) {
+  var keys = Object.keys(acts).sort().reverse();
+  for (var i = 0; i < keys.length; i++) {
+    var key = keys[i];
+      console.log("KEY:", key);
+  // for (var key in acts) {
       var act = acts[key];
       var row = document.createElement('div');
           row.className = `activity-row ${act.category}`;
@@ -434,7 +346,7 @@ listActivities: function() {
           divImg.className = 'activity-item';
       var actImg = document.createElement('img');
           actImg.className = 'activity-icon';
-          actImg.src = `img/${app.activities[act.key].icon}.png`;
+          //actImg.src = `img/${app.activities[act.key].icon}.png`;
       var divEnj = document.createElement('div');
           divEnj.className = 'activity-item';
           divEnj.setAttribute("onclick",`app.changeEnj("${key}")`);
@@ -451,6 +363,148 @@ listActivities: function() {
   }
 },
 
+
+submitHHid: function() {
+    // Special submission case for non UK participants via HH id
+    var hhID = $("input#free-text").val();
+    localStorage.removeItem('metaID');          // get new metaID
+    localStorage.setItem('household_id', hhID);
+    localStorage.removeItem('householdStatus'); // for connection manager "not linked yet"
+    linkHousehold(hhID);
+
+    localStorage.setItem('householdSurvey', 'COMPLETE');
+    app.returnToMainScreen();
+    app.title.html("HH ID set to " + hhID);
+},
+
+
+toggleCaption: function() {
+  app.btnCaption.toggle();
+  app.helpCaption.toggle();
+},
+
+
+emailData: function() {
+  if (localStorage.getItem('Online') == "true"){
+    requestEmailData();
+    app.title.html(app.label.titleEmailData);
+    app.navigateTo("home");
+    $("input#free-text").val("");
+  } else {
+    app.title.html(app.label.noInternet);
+  }
+},
+
+showHelp: function() {
+  if (localStorage.getItem('Online') == 'true') {
+    var idMeta = localStorage.getItem('metaID');
+    var helpURL = app.label.helpURL;
+    app.title.html("Help"); 
+    app.choicesPane.hide();
+    app.server.show(); 
+    app.server.attr('src', helpURL);
+    app.server.load(function(){
+        sendMessageIframe("App requested help");
+    });
+  } else {
+    app.title.html(app.label.noInternet);
+  }
+},
+
+showProfile: function() {
+  if (localStorage.getItem('Online') == 'true') {
+    var idMeta = localStorage.getItem('metaID');
+    var profileURL = app.label.profileURL + idMeta;
+    console.log("URL:" + profileURL);
+    app.header.hide(); 
+    app.choicesPane.hide();
+    app.server.show(); 
+    app.server.attr('src', profileURL);
+    app.server.load(function(){
+        sendMessageIframe("App requested profile");
+    });
+  } else {
+    app.title.html(app.label.noInternet);
+  }
+},
+
+showEnjoyment: function() {
+  var activityList = utils.getList(ACTIVITY_LIST) || []
+  var actCount = Object.keys(activityList).length;
+  if (actCount == 0) {
+    app.title.html(app.label.noActivities);
+  }
+  else if (localStorage.getItem('Online') != null) {
+    var idMeta = localStorage.getItem('metaID');
+    var enjoymentURL = app.label.enjoymentURL + idMeta;
+    app.header.hide(); 
+    app.choicesPane.hide();
+    app.server.show(); 
+    app.server.attr('src', enjoymentURL);
+    app.server.load(function(){
+        sendMessageIframe("App requested enjoyment");
+    });
+  } else {
+    app.title.html(app.label.noInternet);
+  }
+},
+
+};
+
+app.initialize();
+
+
+/*
+
+statusCheck: function() {
+    // steps are                    condition
+    // 1: personalise               hh id
+    // 2: finish personalise        registration status = 'complete'
+    // 3: get date                  dateChoice
+    // 4: survey                    SURVEY_STATUS
+    // 5: study (stars);
+    // 6: return eMeter             date > dateChoice +1
+    
+    var dateChoice = localStorage.getItem('dateChoice');
+    var hh = localStorage.getItem('household_id');
+    var sc = localStorage.getItem('sc');
+    var waitForAuthorisation = localStorage.getItem('AwaitAuthorisation');
+
+  // Individual Survey
+  if (localStorage.getItem("survey root") == 'survey complete') {
+    app.activities['IndividualSurvey']['icon'] = "act3_user_green";
+  }
+
+  if (localStorage.getItem("householdSurvey") == 'COMPLETE') {
+    app.activities['Register']['icon'] = "act3_home_green";
+    if (localStorage.getItem("survey root") == 'survey complete') {
+        app.imgStatus.attr("src","img/act3_user_green.png");
+    }
+  }
+},
+
+showStars: function() {
+    // activityList = utils.getList(ACTIVITY_LIST) || {};
+    // var actCount = Object.keys(activityList).length;
+    var actCount = localStorage.getItem('ActivityCount');
+    if (actCount > 4) {
+          app.imgStatus.attr("src","img/act3_user_star.png");
+    }
+    //if (actCount > 24) {
+    //  app.imgStatus.attr("src","img/stars_5.png");
+    //} else if (actCount > 14) {
+    //  app.imgStatus.attr("src","img/stars_4.png");
+    //} else if (actCount > 9) {
+    //  app.imgStatus.attr("src","img/stars_3.png");
+    //} else if (actCount > 4) {
+    //  app.imgStatus.attr("src","img/stars_2.png");
+    //} else if (actCount > 0) {
+    //  app.imgStatus.attr("src","img/stars_1.png");
+    //} else {
+    //  app.imgStatus.attr("src","img/stars_0.png");
+    //}
+    //app.imgStatus.show();
+},
 showActivityList: function() {
   // only used on home screen
   // used as a proxy for 'being home'
@@ -551,92 +605,4 @@ addActivityToList: function() {
       localStorage.setItem('ActivityCount', 1)
   }
 },
-
-submitHHid: function() {
-    // Special submission case for non UK participants via HH id
-    var hhID = $("input#free-text").val();
-    localStorage.removeItem('metaID');          // get new metaID
-    localStorage.setItem('household_id', hhID);
-    localStorage.removeItem('householdStatus'); // for connection manager "not linked yet"
-    linkHousehold(hhID);
-
-    localStorage.setItem('householdSurvey', 'COMPLETE');
-    app.returnToMainScreen();
-    app.title.html("HH ID set to " + hhID);
-},
-
-
-toggleCaption: function() {
-  app.btnCaption.toggle();
-  app.helpCaption.toggle();
-},
-
-
-emailData: function() {
-  if (localStorage.getItem('Online') == "true"){
-    requestEmailData();
-    app.title.html(app.label.titleEmailData);
-    app.navigateTo("home");
-    $("input#free-text").val("");
-  } else {
-    app.title.html(app.label.noInternet);
-  }
-},
-
-showHelp: function() {
-  if (localStorage.getItem('Online') == 'true') {
-    var idMeta = localStorage.getItem('metaID');
-    var helpURL = app.label.helpURL;
-    app.title.html("Help"); 
-    app.choicesPane.hide();
-    app.server.show(); 
-    app.server.attr('src', helpURL);
-    app.server.load(function(){
-        sendMessageIframe("App requested help");
-    });
-  } else {
-    app.title.html(app.label.noInternet);
-  }
-},
-
-showProfile: function() {
-  if (localStorage.getItem('Online') == 'true') {
-    var idMeta = localStorage.getItem('metaID');
-    var profileURL = app.label.profileURL + idMeta;
-    console.log("URL:" + profileURL);
-    app.header.hide(); 
-    app.choicesPane.hide();
-    app.server.show(); 
-    app.server.attr('src', profileURL);
-    app.server.load(function(){
-        sendMessageIframe("App requested profile");
-    });
-  } else {
-    app.title.html(app.label.noInternet);
-  }
-},
-
-showEnjoyment: function() {
-  var activityList = utils.getList(ACTIVITY_LIST) || []
-  var actCount = Object.keys(activityList).length;
-  if (actCount == 0) {
-    app.title.html(app.label.noActivities);
-  }
-  else if (localStorage.getItem('Online') != null) {
-    var idMeta = localStorage.getItem('metaID');
-    var enjoymentURL = app.label.enjoymentURL + idMeta;
-    app.header.hide(); 
-    app.choicesPane.hide();
-    app.server.show(); 
-    app.server.attr('src', enjoymentURL);
-    app.server.load(function(){
-        sendMessageIframe("App requested enjoyment");
-    });
-  } else {
-    app.title.html(app.label.noInternet);
-  }
-},
-
-};
-
-app.initialize();
+*/
